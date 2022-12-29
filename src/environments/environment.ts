9947d7a477cf692068af8572f43e36abce45fe5d
Martin Dagarin <martin.dagarin@gmail.com>
2022-12-29 15:26:40 +0100

    URL fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   apiPotUrl: ((<any> window).env.apiPotUrl) || 'http://localhost:3003',
   apiTipVozilUrl: ((<any> window).env.apiTipVozilUrl) || 'http://localhost:3000',
   apiUporabnikiUrl: ((<any> window).env.apiUporabnikiUrl) || 'http://localhost:3002',
-  apiVozilaUrl: ((<any> window).env.apiVozilaUrl) || 'http://localhost:3001',
+  apiVozilaUrl: ((<any> window).env.apiVozilaUrl) || 'http://localhost:3002',
 };
 
 /*
